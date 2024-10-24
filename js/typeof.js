896a45672589e2dae6be8d4ff3a2f20062b80917
DavidFraga09 <2302079@utrivieramaya.edu.mx>
2024-10-23 22:01:49 -0500

    Update typeof.js

--- js/typeof.js
+++ js/typeof.js
@@ -1,5 +1,5 @@
 console.log(typeof 'Yo') //string
 console.log(typeof 25)  // number
 console.log(typeof true) //boolean
-console.log(typeof null) // object type
-console.log(typeof undefined) //
+console.log(typeof null) // null
+console.log(typeof undefined) // undefined
