402b3179fb73dc0051cff0ab9ffe387f7aba0416
DavidFraga09 <2302079@utrivieramaya.edu.mx>
2024-10-23 22:01:00 -0500

    Add files via upload

---
+++ js/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof 'David') 
+console.log(typeof true) 
+console.log(typeof undefined) 
+console.log(typeof null) 
---
+++ js/variable.js
@@ -0,0 +1,42 @@
+// Los comentarios pueden hacer que el codigo sea legible
+// Este es mi primer codigo js
+/*Los comentarios pueden hacer que el codigo sea legible*/
+let gg = 'ElMamo';  //String
+let status = false; //boolean
+let city; //undefined
+let address = null;
+
+console.log('David') 
+console.log(true) 
+console.log(undefined) 
+console.log(null)
+
+let uno;
+let dos;
+let tres;
+let cuatro;
+
+let color = "azul";
+let edad = 25;
+let ci = "Tepeji";
+let state = Hidalgo;
+
+let MyName = 'David'
+let myLastName = 'Dominguez'
+let isMarried = false;
+let country = 'Mexico'
+let age = '21';
+
+
+let myName1, myLastName1, isMarried1, country1, age1;
+
+name1 = 'Adrian';
+name1 = 'Cecilia';
+name1 = 'Sebastian';
+
+let myAge = 21;
+let yourAge = 25;
+console.log(myAge,yourAge);
+
+console.log('Mi edad es' + ' ' + myAge, 'Tu edad es' + ' ' + yourAge);
+
